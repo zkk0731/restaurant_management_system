2dcb393e6b60b075d273f1fbd00423748fa68fb9
yu <author@example>
2022-12-06 16:40:18 +0800

    PointsExchange(mysql, entity, dao)

---
+++ src/main/java/com/example/restaurant_management_system/repository/PointsDao.java
@@ -0,0 +1,11 @@
+package com.example.restaurant_management_system.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.restaurant_management_system.entity.Points;
+
+@Repository
+public interface PointsDao extends JpaRepository<Points, Integer> {
+
+}
